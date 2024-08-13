929fd69aa5cdd9debdd7874d009cb8efe0e311b3
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2024-08-13 17:07:34 +0530

    Create Document & Query.py

---
+++ Document & Query.py
@@ -0,0 +1,12 @@
+documents = [
+    "This is a list which containing sample documents.",
+    "Keywords are important for keyword-based search.",
+    "Document analysis involves extracting keywords.",
+    "Keyword-based search relies on sparse embeddings.",
+    "Understanding document structure aids in keyword extraction.",
+    "Efficient keyword extraction enhances search accuracy.",
+    "Semantic similarity improves document retrieval performance.",
+    "Machine learning algorithms can optimize keyword extraction methods."
+]
+
+query = "Natural language processing techniques enhance keyword extraction efficiency."
